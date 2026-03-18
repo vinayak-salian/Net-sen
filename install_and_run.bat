305dd6ea3a03a05183b60522c4420ef11beba7ce
vinayak-salian <139992655+vinayak-salian@users.noreply.github.com>
2026-03-18 16:08:56 +0530

    Update install_and_run.bat

--- install_and_run.bat
+++ install_and_run.bat
@@ -1,1 +1,11 @@
 
+@echo off
+echo [*] Initializing NetSentinel Core Installation...
+echo [*] Installing required dependencies...
+
+pip install -r requirements.txt
+
+echo [*] Launching NetSentinel Agent with Administrator Privileges...
+python agent.py
+
+pause
